69443ae45f87807e14feb57d0da6670e7a7ca8fb
retelesk <tranhongquan.jb@gmail.com>
2025-04-10 20:24:20 +0700

    thêm nút back-to-top

--- js/homePage.js
+++ js/homePage.js
@@ -6,4 +6,21 @@ document.addEventListener("DOMContentLoaded", function () {
         .getElementById("header-search-form")
         .classList.toggle("disabled");
     });
+  // BACK-TO-TOP
+  const backToTopBtn = document.getElementById("backToTopBtn");
+
+  window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 300) {
+      backToTopBtn.classList.add("active");
+    } else {
+      backToTopBtn.classList.remove("active");
+    }
+  });
+
+  backToTopBtn.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
 });
