4bf80bc2b2b1d6d5f148ce760e98871a81b4ea31
PhongVu0705 <pvu0705@gmail.com>
2025-04-14 20:41:30 +0700

    udpate search page - completed

--- js/search.js
+++ js/search.js
@@ -1,10 +1,12 @@
 "use strict";
-
+//****************Global Variables********************** */
 const url = new URL(window.location.href);
 const keyword = url.searchParams.get("keyword");
+var data = [];
+let newsData = [];
 
+//****************Main function********************** */
 document.querySelector("#keyword").innerHTML = keyword;
-
 document
   .getElementById("page-search-btn")
   .addEventListener("click", function () {
@@ -14,3 +16,80 @@ document
     }
     window.location.href = "./search.html?keyword=" + searchValue;
   });
+document
+  .querySelector("#page-search-input")
+  .addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+      let searchValue = document.querySelector("#page-search-input").value;
+      if (searchValue == "") {
+        searchValue = " ";
+      }
+      location.href = "./search.html?keyword=" + searchValue;
+    }
+  });
+
+async function main() {
+  await loadData();
+  const lowercaseKeyword = keyword.toLowerCase();
+  newsData = data.filter((item) => {
+    return item.title.toLowerCase().includes(lowercaseKeyword);
+  });
+
+  // Display results count
+  if (newsData.length == 0) {
+    document.querySelector("#search-result-text").textContent =
+      "No result found";
+  } else {
+    document.querySelector("#search-result-text").textContent =
+      newsData.length + " results found";
+    // Display the search results
+    displaySearchResults(newsData);
+  }
+}
+// Call the main function
+main();
+
+//****************Common function********************** */
+// Get data from JSON file
+async function loadData() {
+  try {
+    // Fetch data from the JSON file
+    const response = await fetch("../data/dataSample.json");
+    data = await response.json();
+  } catch (error) {
+    console.error("Error loading JSON data:", error);
+  }
+}
+
+// Function to search id in the data
+function findObjectById(array, id) {
+  return array.find((obj) => obj.id == id);
+}
+
+// Function to display search results
+function displaySearchResults(results) {
+  const searchResultsContainer = document.getElementById("result-area");
+
+  // Clear any existing content
+  searchResultsContainer.innerHTML = "";
+
+  // Map through results and create HTML elements
+  const resultsHTML = results
+    .map((item) => {
+      return `
+      <div class="search-result-item">
+        <div class="search-result-item-thumb">
+          <img src="${item.thumbnail}" alt="news-img" />
+        </div>
+        <h3 class="search-result-item-title">
+          ${item.title}
+        </h3>
+        <p class="search-result-item-date">May 10, 2016</p>
+      </div>
+    `;
+    })
+    .join(""); // Join all HTML strings into one
+
+  // Insert the HTML into the container
+  searchResultsContainer.innerHTML = resultsHTML;
+}
